a1f84bf5eacb359b48b402e9365817cc003f86cc
konstantgr <konstantin.grotov@gmail.com>
2021-07-08 15:24:49 +0300

    Second attempt

--- db_structures.py
+++ db_structures.py
@@ -1,54 +1,63 @@
 from sqlalchemy import Column, String, Integer, Text, Boolean, ForeignKey
+from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()        
+base = declarative_base()
 
-class Notebook(base):
+
+class NotebookDb(base):
     __tablename__ = 'Notebook'
     notebook_id = Column(Integer, primary_key=True)
     notebook_name = Column(String, unique=True)
-    notebook_language  = Column(String)
-    notebook_version  = Column(String)
-    
+    notebook_language = Column(String)
+    notebook_version = Column(String)
 
-class Cell(base):
+
+class CellDb(base):
     __tablename__ = 'Cell'
     cell_id = Column(Integer, primary_key=True)
     notebook_id = Column(Integer, ForeignKey('Notebook.notebook_id'))
-    
 
-class Code_cell(base):
+
+class CodeCellDb(base):
     __tablename__ = 'Code_cell'
     cell_id = Column(Integer, ForeignKey('Cell.cell_id'),
                      primary_key=True)
     cell_num = Column(Integer)
-    code_imports = Column(Text)
-    code_lines_count = Column(Integer)
-    code_chars_count = Column(Integer)
+    code_imports = Column(Text, default='')
+    code_instructions_count = Column(Integer, default=0)
+    code_lines_count = Column(Integer, default=0)
+    code_chars_count = Column(Integer, default=0)
     source = Column(Text)
 
-              
-class Md_cell(base):
+
+class MdCellDb(base):
     __tablename__ = 'Md_cell'
-    cell_id = Column(Integer, ForeignKey('Cell.cell_id'), 
+    cell_id = Column(Integer, ForeignKey('Cell.cell_id'),
                      primary_key=True)
     cell_num = Column(Integer)
-    sentences_count = Column(Integer)
-    words_count = Column(Integer)
-    unique_words = Column(Text)
-    cell_language = Column(String)
-    latex = Column(Boolean)
-    html = Column(Boolean)
-    code = Column(Boolean)
+    sentences_count = Column(Integer, default=0)
+    words_count = Column(Integer, default=0)
+    unique_words = Column(Text, default='')
+    cell_language = Column(String, default='')
+    latex = Column(Boolean, default=False)
+    html = Column(Boolean, default=False)
+    code = Column(Boolean, default=False)
     source = Column(Text)
-    
-              
-class Notebook_features(base):
+
+
+class NotebookFeaturesDb(base):
     __tablename__ = 'Notebook_features'
-    notebook_id = Column(Integer, ForeignKey('Notebook.notebook_id'), 
+    notebook_id = Column(Integer, ForeignKey('Notebook.notebook_id'),
                          primary_key=True)
     notebook_cells_number = Column(Integer)
     md_cells_count = Column(Integer)
     code_cells_count = Column(Integer)
     notebook_imports = Column(Text)
+
+
+def create_db(name):
+    engine = create_engine(f'sqlite:///{name}', echo=True)
+    base.metadata.drop_all(engine)
+    base.metadata.create_all(engine)
--- index.py
+++ index.py
@@ -2,24 +2,51 @@ import json
 import time
 from multiprocessing import Pool
 from tqdm import tqdm
+import numpy as np
+from Notebook import Notebook
+from db_structures import create_db
 
-from notebook_features import get_ntb
+def add_notebook(name):
+    #config = {'get_lines_of_code': True, 'get_cell_language': False}
+    config = {
+        'code_instructions_count': True,
+        'code_lines_count': True,
+        'cell_language': False,
+        'code_imports': True}
+
+    try:
+        nb = Notebook(name)
+        log = nb.parse_features(config)
+        rows = nb.write_to_db()
+        return rows
+    except Exception as e:
+        with open("log.txt", "a") as f:
+            f.write(f'{name}:\t{repr(e)}\n')
+        return 0
+
 
 def main():
     with open('ntbs_list.json', 'r') as fp:
-        start, step = 5000, 1000
+        start, step = 0, 5
         ntb_list = json.load(fp)[start:start+step]
 
+    create_db('test.db')
+    res = []
     with tqdm(total=len(ntb_list)) as pbar:
-        with Pool(processes=6) as pool:
-            for stats in pool.imap_unordered(get_ntb, ntb_list, 
-                                             chunksize=1):
+        with Pool(processes=5) as pool:
+            for result in pool.imap_unordered(add_notebook, ntb_list,
+                                              chunksize=1):
+                res.append(result)
                 pbar.update(1)
 
     print('Finishing...')
+    print('{} notebooks contain errors ({:.1f}%) '.format(
+        len(res) - sum(res),
+        (len(res) - sum(res)) / len(res) * 100
+    ))
 
 
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- {:.1f} seconds ---".format(time.time() - start_time))
